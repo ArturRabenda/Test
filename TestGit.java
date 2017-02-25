b65cdeb144e18b2ac9c89f9b1273ce205af28ad9
Artur Rabenda <rabenda27178@student.pwsztar.edu.pl>
2017-02-26 00:06:22 +0100

    mmmm

--- TestGit.java
+++ TestGit.java
@@ -17,6 +17,7 @@ public class TestGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("3232");
+        System.out.print("2222");
     }
     
 }
