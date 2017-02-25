3e60ec6e461eb6dd220c53cd9c8a387232548a2c
Artur Rabenda <rabenda27178@student.pwsztar.edu.pl>
2017-02-25 23:29:07 +0100

    test 2

--- TestGit.java
+++ TestGit.java
@@ -13,6 +13,7 @@ public class TestGit {
 
     /**
      * @param args the command line arguments
+     * @author Artus
      */
     public static void main(String[] args) {
         // TODO code application logic here
